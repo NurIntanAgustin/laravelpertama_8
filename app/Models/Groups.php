3ad06cc5bdd6340d691cd28a5a4d031b725486be
NurIntanAgustin <nurintan311@gmail.com>
2021-12-21 21:10:38 +0700

    commit tugas 8

--- app/Models/Groups.php
+++ app/Models/Groups.php
@@ -9,13 +9,7 @@ class Groups extends Model
 {
     use HasFactory;
 
-<<<<<<< HEAD
-    protected $fillable = [
-        'name', 'description',
-    ];
-=======
     protected $guarded = ['name', 'total_users_join', 'total_users_leave'];
->>>>>>> 258b9b588e4772ae96b21cb936ab4ca13eb24c2b
 
     public function friends()
     {
